23e39c4648aff73540f16b8c8dd02a098074c082
dpatino2 <dpatino2@gmail.com>
2017-08-27 23:30:48 -0500

    initial commit all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdTabsModule, 
+          MdMenuModule, 
+          MdToolbarModule, 
+          MdIconModule, 
+          MdListModule, 
+          MdButtonModule, 
+          MdCardModule,
+          MdGridListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +18,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdMenuModule,
+    MdIconModule,
+    MdListModule,
+    MdButtonModule,
+    MdCardModule,
+    MdGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
